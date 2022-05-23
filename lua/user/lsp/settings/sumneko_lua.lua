56366d5cbc4e617eb41aec78df8581314bcbcf7e
Mose Mueller <mosmuell@ethz.ch>
2022-05-23 09:11:56 +0200

    chore(lsp): remove usage of deprecated lsp_installer.on_server_ready()

--- lua/user/lsp/settings/sumneko_lua.lua
+++ lua/user/lsp/settings/sumneko_lua.lua
@@ -2,11 +2,17 @@ return {
     settings = {
 
         Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT',
+            },
             diagnostics = {
-                globals = { "vim" },
+                -- Get the language server to recognize the `vim` global
+                globals = {'vim'},
             },
             workspace = {
-                library = {
+                -- Make the server aware of Neovim runtime files
+                library = { vim.api.nvim_get_runtime_file("", true),
                     [vim.fn.expand("$VIMRUNTIME/lua")] = true,
                     [vim.fn.stdpath("config") .. "/lua"] = true,
                 },
