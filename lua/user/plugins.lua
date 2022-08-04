613c87e06242c2ddc9f2e6bbc0dc5d798f204e48
Mose Mueller <mosmuell@ethz.ch>
2022-08-04 19:53:59 +0200

    chore: formatting

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -2,11 +2,11 @@ local fn = vim.fn
 -- Automatically install packer
 local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then PACKER_BOOTSTRAP = fn.system { "git", "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-}
+        "--depth",
+        "1",
+        "https://github.com/wbthomason/packer.nvim",
+        install_path,
+    }
     print "Installing packer close and reopen Neovim..."
     vim.cmd [[packadd packer.nvim]]
 end
@@ -125,7 +125,7 @@ return packer.startup(function(use)
     use {
         'iamcco/markdown-preview.nvim',
         run = function() vim.fn['mkdp#util#install']() end,
-        ft = {'markdown'}
+        ft = { 'markdown' }
     }
 
     -- Automatically set up your configuration after cloning packer.nvim
