465d82b1292aa0766a4cb3f3cc06ae1da7d25f64
Mose Mueller <mosmuell@ethz.ch>
2023-06-02 15:04:26 +0200

    updating pyright lsp config

--- lua/user/lsp/settings/pyright.lua
+++ lua/user/lsp/settings/pyright.lua
@@ -1,26 +1,12 @@
-local util = require("lspconfig/util")
-local python_root_files = {
-    'WORKSPACE', -- added for Bazel; items below are from default config
-    'pyproject.toml',
-    'setup.py',
-    'setup.cfg',
-    'requirements.txt',
-    'Pipfile',
-    'pyrightconfig.json',
+return {
+	settings = {
+		python = {
+			analysis = {
+				autoSearchPaths = true,
+				diagnosticMode = "workspace",
+				useLibraryCodeForTypes = true,
+				typeCheckingMode = "on",
+			},
+		},
+	},
 }
-
-local opts = {
-    setup = {
-        root_dir = util.root_pattern(unpack(python_root_files)),
-        settings = {
-            pyright = {
-                autoSearchPaths = true,
-                diagnosticMode = "workspace",
-                useLibraryCodeForTypes = true
-            },
-        },
-        single_file_support = true
-    },
-}
-
-return opts
