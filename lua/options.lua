964be6cd38e6e785b2369c3fbfd77015a3c66425
Mose Mueller <mosmuell@ethz.ch>
2023-09-25 18:01:03 +0200

    feat: updating vim options (using zsh as vim shell, tabstop, shiftwidth)

--- lua/options.lua
+++ lua/options.lua
@@ -22,8 +22,8 @@ vim.opt.undofile = true -- enable persistent undo
 vim.opt.updatetime = 300 -- faster completion (4000ms default)
 vim.opt.writebackup = false -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
 vim.opt.expandtab = true -- convert tabs to spaces
-vim.opt.shiftwidth = 2 -- the number of spaces inserted for each indentation
-vim.opt.tabstop = 4 -- insert 2 spaces for a tab
+vim.opt.shiftwidth = 4 -- the number of spaces inserted for each indentation
+vim.opt.tabstop = 4 -- insert 4 spaces for a tab
 vim.opt.cursorline = true -- highlight the current line
 vim.opt.number = true -- set numbered lines
 vim.opt.laststatus = 3 -- only the last window will always have a status line
@@ -41,4 +41,5 @@ vim.opt.whichwrap:append "<,>,[,],h,l" -- keys allowed to move to the previous/n
 vim.opt.iskeyword:append "-" -- treats words with `-` as single words
 vim.opt.formatoptions:remove { "c", "r", "o" } -- This is a sequence of letters which describes how automatic formatting is to be done
 vim.opt.linebreak = true
+vim.opt.shell = "/usr/bin/zsh"
 vim.g.mapleader = " "
