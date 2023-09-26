389fcd3328c8a40fd4b20afde712a82b9510ffa8
Mose Mueller <mosemueller@gmail.com>
2023-09-26 09:55:00 +0200

    feat: adding neodev and neorepl plugins (for lua development)

---
+++ lua/user/neodev.lua
@@ -0,0 +1,7 @@
+local M = {
+  "folke/neodev.nvim",
+  commit = "e683583f8ba2f9dbbc4539889cbbdbfc5afb5fb2",
+  event = "VimEnter",
+}
+
+return M
---
+++ lua/user/neorepl.lua
@@ -0,0 +1,7 @@
+local M = {
+  "ii14/neorepl.nvim",
+  commit = "bc819bb42edca9c4a6b6e5d00f09f94a49c3b735",
+  event = "VimEnter",
+}
+
+return M
