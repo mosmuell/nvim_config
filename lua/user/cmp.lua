b2772a250ca3e89ae6833cc136b1d92a42ad880f
Mose Mueller <mosmuell@ethz.ch>
2023-10-22 21:40:08 +0200

    Updates cmp keybindings

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -89,8 +89,8 @@ function M.config()
       end,
     },
     mapping = cmp.mapping.preset.insert {
-      ["<C-k>"] = cmp.mapping.select_prev_item(),
-      ["<C-j>"] = cmp.mapping.select_next_item(),
+      -- ["<C-k>"] = cmp.mapping.select_prev_item(),
+      -- ["<C-j>"] = cmp.mapping.select_next_item(),
       ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
       ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
       ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
@@ -100,7 +100,8 @@ function M.config()
       },
       -- Accept currently selected item. If none selected, `select` first item.
       -- Set `select` to `false` to only confirm explicitly selected items.
-      ["<CR>"] = cmp.mapping.confirm { select = true },
+      ["<CR>"] = cmp.mapping.confirm { select = false },
+      ["<Right>"] = cmp.mapping.confirm { select = true },
       ["<Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_next_item()
