e65e71a06ba074dceb3690c2fd8a1df6342d94d8
Mose Mueller <mosmuell@ethz.ch>
2023-11-10 21:20:38 +0100

    toggleterm: removes <esc> keybinding

--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -34,7 +34,7 @@ function M.config()
 
   function _G.set_terminal_keymaps()
     local opts = { noremap = true }
-    vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
+    -- vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
     vim.api.nvim_buf_set_keymap(0, "t", "<M-h>", [[<C-\><C-n><C-W>h]], opts)
     vim.api.nvim_buf_set_keymap(0, "t", "<M-j>", [[<C-\><C-n><C-W>j]], opts)
     vim.api.nvim_buf_set_keymap(0, "t", "<M-k>", [[<C-\><C-n><C-W>k]], opts)
