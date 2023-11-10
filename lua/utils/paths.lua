fd31b97440e80fc923deb0413075581169faea05
Mose Mueller <mosmuell@ethz.ch>
2023-11-10 21:18:12 +0100

    adds utils.paths module containing venv paths methods

---
+++ lua/utils/paths.lua
@@ -0,0 +1,25 @@
+local M = {}
+M.get_local_venv_dir = function()
+  local cwd = vim.fn.getcwd()
+
+  -- Check for ".venv" and "venv" directories
+  local venv_paths = { ".venv", "venv" }
+  for _, venv_path in ipairs(venv_paths) do
+    local full_path = cwd .. "/" .. venv_path
+
+    -- Use io.open to check if the directory exists
+    local f, err = io.open(full_path, "r")
+    if f then
+      f:close()
+      return full_path
+    end
+  end
+
+  return nil
+end
+
+M.get_venv_or_local_venv_dir = function()
+  local venv_path = os.getenv "VIRTUAL_ENV" or M.get_local_venv_dir()
+  return venv_path
+end
+return M
