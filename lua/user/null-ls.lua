aa15189300600e825cd919af1b227d11a9b23895
Mose Mueller <mosemueller@gmail.com>
2023-11-10 13:39:15 +0100

    null-ls: using black, isort and flake8 executables from activated venv

--- lua/user/null-ls.lua
+++ lua/user/null-ls.lua
@@ -10,6 +10,29 @@ local M = {
   },
 }
 
+local function get_relative_venv_path()
+  local venv_path = os.getenv "VIRTUAL_ENV"
+  if venv_path == nil then
+    return ""
+  end
+
+  local cwd = vim.fn.getcwd()
+
+  -- Count the number of "/" in cwd
+  local slash_count = 0
+  for _ in string.gmatch(cwd, "/") do
+    slash_count = slash_count + 1
+  end
+
+  -- Create a string with the same number of "../"
+  local relative_path = string.rep("../", slash_count)
+
+  -- Append the venv_path to the relative path
+  local full_path = relative_path .. venv_path
+
+  return full_path
+end
+
 function M.config()
   local null_ls = require "null-ls"
   -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -21,13 +44,17 @@ function M.config()
     debug = false,
     sources = {
       formatting.prettierd.with { extra_filetypes = { "toml" } },
+      formatting.black.with {
+        prefer_local = get_relative_venv_path() .. "/bin",
+        extra_args = { "--fast" },
       },
-      formatting.black.with { extra_args = { "--fast" } },
-      formatting.isort,
+      formatting.isort.with { prefer_local = get_relative_venv_path() .. "/bin" },
       formatting.stylua,
       -- formatting.google_java_format,
-      diagnostics.flake8,
       diagnostics.eslint_d,
+      diagnostics.flake8.with {
+        prefer_local = get_relative_venv_path() .. "/bin",
+      },
       diagnostics.cpplint,
     },
   }
