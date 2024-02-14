1169c28b1a9d3490008011250fff98564d0eecdc
Mose Mueller <mosemueller@gmail.com>
2024-02-14 15:44:25 +0100

    moves lsp mappings to keymaps.lua

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -180,7 +180,9 @@ customKeymap("n", "<F41>", "<cmd>lua require'dap'.run_last()<cr>", "Restart") --
 
 -- Lsp
 customKeymap("n", "<leader>lf", "<cmd>lua vim.lsp.buf.format{ async = true }<cr>", "Format file")
-customKeymap("n", "<leader>lR", ":LspRestart<CR>", "Restart LSP")
+customKeymap("n", "<leader>lR", "<CMD>LspRestart<CR>", "Restart LSP")
+customKeymap("n", "<leader>li", "<CMD>LspInfo<CR>", "LSP Info")
+customKeymap("n", "<leader>lI", "<CMD>Mason<CR>", "Mason")
 
 -- Toggleterm
 customKeymap("n", "<leader>t", "<cmd>:ToggleTerm<cr>", "Open Toggleterm terminal")
