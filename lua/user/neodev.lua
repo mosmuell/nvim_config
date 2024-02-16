5680c93696553a444f0fb192aea145bd9107c3b6
Mose Mueller <mosmuell@ethz.ch>
2024-02-16 14:48:08 +0100

    neodev: fixes setup

--- lua/user/neodev.lua
+++ lua/user/neodev.lua
@@ -1,7 +1,8 @@
 local M = {
   "folke/neodev.nvim",
-  commit = "e683583f8ba2f9dbbc4539889cbbdbfc5afb5fb2",
-  event = "VimEnter",
+  config = function()
+    require("neodev").setup()
+  end,
 }
 
 return M
