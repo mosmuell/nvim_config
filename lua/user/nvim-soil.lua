70b593b65dfc6f71bcb8ce5a4fb02206fd39cf9a
Mose Mueller <mosemueller@gmail.com>
2024-02-16 12:20:29 +0100

    nvim-soil: updates config
    I will probably create my own plugin for this at some point.

--- lua/user/nvim-soil.lua
+++ lua/user/nvim-soil.lua
@@ -1,4 +1,4 @@
-return {
+M = {
   "javiorfo/nvim-soil",
   lazy = true,
   ft = "plantuml",
@@ -13,14 +13,7 @@ return {
         darkmode = false, -- Enable or disable darkmode
         format = "svg", -- Choose between png or svg
 
-        -- This is a default implementation of using nsxiv to open the resultant image
-        -- Edit the string to use your preferred app to open the image
-        -- Some examples:
-        -- return "feh " .. img
-        -- return "xdg-open " .. img
         execute_to_open = function(img)
-          -- return "feh --conversion-timeout 1 " .. img
-          -- return "eog " .. img
           return 0
         end,
       },
@@ -31,45 +24,20 @@ return {
     -- Create or get an autocommand group for PlantUML setup
     local augroup = vim.api.nvim_create_augroup("PlantUMLSetup", { clear = true })
 
-    local generate_svg_file = function()
-      local settings = require("soil").DEFAULTS
-
-      local puml_jar = settings.puml_jar
-      local file_with_extension = vim.fn.expand "%:p"
-      local format = settings.image.format
-      local darkmode = settings.image.darkmode and "-darkmode" or ""
-
-      local handle
-      local args = { "-jar", puml_jar, "-t" .. format, file_with_extension }
-      if settings.image.darkmode then
-        table.insert(args, "-darkmode")
-      end
-
-      handle = vim.loop.spawn(
-        "java",
-        {
-          args = args,
-          stdio = { nil, nil, nil }, -- You can redirect input, output, and error if needed
-        },
-        vim.schedule_wrap(function(return_code)
-          if return_code ~= 0 then
-            vim.cmd "redraw"
-            vim.notify("Failed to generate PlantUML SVG", vim.log.levels.ERROR)
-          end
-          handle:close()
-        end)
-      )
-    end
-
-    -- -- Use an autocommand to set the keymap for plantuml filetype buffers
-    -- vim.api.nvim_create_autocmd("FileType", {
-    --   group = augroup,
-    --   pattern = "plantuml",
-    --   callback = function(args)
-    --     -- Set custom keymap for generating PlantUML SVG image
-    --     customKeymap("n", "<leader>s", '<CMD>lua require("soil.core").run()<CR>', "Generate PlantUML svg image")
-    --   end,
-    -- })
+    -- Use an autocommand to set the keymap for plantuml filetype buffers
+    vim.api.nvim_create_autocmd("FileType", {
+      group = augroup,
+      pattern = "plantuml",
+      callback = function(args)
+        -- Set custom keymap for generating PlantUML SVG image
+        customKeymap(
+          "n",
+          "<leader>s",
+          '<CMD>lua require("user.nvim-soil").open_image()<CR>',
+          "Generate PlantUML svg image"
+        )
+      end,
+    })
 
     -- Execute the command upon writing the file
     vim.api.nvim_create_autocmd("BufWritePost", {
@@ -77,8 +45,75 @@ return {
       pattern = { "*.puml" },
       callback = function()
         -- Execute the command to generate PlantUML SVG image
-        generate_svg_file()
+        M.generate_svg_file()
+      end,
+    })
+
+    vim.api.nvim_create_autocmd("VimLeavePre", {
+      group = augroup,
+      callback = function()
+        if vim.g.nvim_soil_feh_pid then
+          -- Use a system command to kill the feh process
+          vim.fn.jobstart { "kill", vim.g.nvim_soil_feh_pid }
+          vim.g.nvim_soil_feh_pid = nil -- Clear the PID
+        end
       end,
     })
   end,
 }
+
+M.generate_svg_file = function()
+  local settings = require("soil").DEFAULTS
+  local puml_jar = settings.puml_jar
+  local file_with_extension = vim.fn.expand "%:p"
+  local format = settings.image.format
+
+  local handle
+  local args = { "-jar", puml_jar, "-t" .. format, file_with_extension }
+  if settings.image.darkmode then
+    table.insert(args, "-darkmode")
+  end
+
+  vim.loop.spawn(
+    "java",
+    {
+      args = args,
+      stdio = { nil, nil, nil }, -- You can redirect input, output, and error if needed
+    },
+    vim.schedule_wrap(function(return_code)
+      if return_code ~= 0 then
+        vim.cmd "redraw"
+        vim.notify("Failed to generate PlantUML SVG", vim.log.levels.ERROR)
+      end
+    end)
+  )
+end
+
+M.open_image = function()
+  local settings = require("soil").DEFAULTS
+  local file = vim.fn.expand "%:p:r"
+  local image_file = string.format("%s.%s", file, settings.image.format)
+
+  if vim.g.nvim_soil_feh_pid == nil then
+    local handle
+    handle = vim.loop.spawn(
+      "feh",
+      {
+        args = { "--keep-zoom-vp", "-R", "1", "--conversion-timeout", "1", image_file },
+        stdio = { nil, nil, nil },
+      },
+      vim.schedule_wrap(function(return_code)
+        vim.g.nvim_soil_feh_pid = nil -- Clear the PID
+        if return_code ~= 0 then
+          vim.cmd "redraw"
+          vim.notify("Failed to open PlantUML diagram.", vim.log.levels.ERROR)
+        end
+      end)
+    )
+    -- Store the PID for later use
+    local pid = handle:get_pid()
+    vim.g.nvim_soil_feh_pid = pid -- Use a global variable, or another method to store the PID
+  end
+end
+
+return M
