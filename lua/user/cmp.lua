aa6758b1bc88b8fef578db5a8e324e78a6678f4e
Mose Mueller <mosmuell@ethz.ch>
2024-07-02 11:17:44 +0200

    cmp: remove formatting of completion suggestion window

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -127,21 +127,22 @@ function M.config()
         "s",
       }),
     }),
-    formatting = {
-      fields = { "kind", "abbr", "menu" },
-      format = function(entry, vim_item)
-        vim_item.kind = kind_icons[vim_item.kind]
-        vim_item.menu = ({
-          nvim_lsp = "",
-          nvim_lua = "",
-          luasnip = "",
-          buffer = "",
-          path = "",
-          emoji = "",
-        })[entry.source.name]
-        return vim_item
-      end,
-    },
+    -- formatting = {
+    --   expandable_indicator = true,
+    --   fields = { "kind", "abbr", "menu" },
+    --   format = function(entry, vim_item)
+    --     vim_item.kind = kind_icons[vim_item.kind]
+    --     vim_item.menu = ({
+    --       nvim_lsp = "",
+    --       nvim_lua = "",
+    --       luasnip = "",
+    --       buffer = "",
+    --       path = "",
+    --       emoji = "",
+    --     })[entry.source.name]
+    --     return vim_item
+    --   end,
+    -- },
     sources = {
       { name = "nvim_lsp" },
       { name = "nvim_lua" },
