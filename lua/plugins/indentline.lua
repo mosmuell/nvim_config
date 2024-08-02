c66bedaa80472d6ffd14432660fb8f8aae38b32e
Mose Mueller <mosemueller@gmail.com>
2024-08-02 15:11:34 +0200

    adds indentline

---
+++ lua/plugins/indentline.lua
@@ -0,0 +1,28 @@
+local M = {
+  "lukas-reineke/indent-blankline.nvim",
+  tag = "v3.7.1",
+  event = "BufReadPre",
+}
+
+function M.config()
+  require("ibl").setup({
+    -- indent = { char = "|" },
+    scope = { enabled = true },
+    exclude = {
+      -- buftypes = { "terminal", "nofile", "quickfix", "prompt" }
+      filetypes = {
+        "lspinfo",
+        "packer",
+        "checkhealth",
+        "help",
+        "man",
+        "gitcommit",
+        "TelescopePrompt",
+        "TelescopeResults",
+        "''",
+        "NvimTree",
+      },
+    },
+  })
+end
+return M
