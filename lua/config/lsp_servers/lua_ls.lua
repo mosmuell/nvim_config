9ebc35e630fc29a68fce34e838d8753775ab7ecd
Mose Mueller <mosemueller@gmail.com>
2024-08-05 07:11:15 +0200

    updates lua_ls config

--- lua/config/lsp_servers/lua_ls.lua
+++ lua/config/lsp_servers/lua_ls.lua
@@ -4,18 +4,18 @@ return {
       format = {
         enable = false,
       },
-      --       diagnostics = {
-      --         globals = { "vim" },
-      --       },
-      --       workspace = {
-      --         library = {
-      --           [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-      --           [vim.fn.stdpath("config") .. "/lua"] = true,
-      --         },
-      --       },
-      --       telemetry = {
-      --         enable = false,
-      --       },
+      -- diagnostics = {
+      --   globals = { "vim" },
+      -- },
+      -- workspace = {
+      --   library = {
+      --     [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+      --     [vim.fn.stdpath("config") .. "/lua"] = true,
+      --   },
+      -- },
+      telemetry = {
+        enable = false,
+      },
     },
   },
 }
