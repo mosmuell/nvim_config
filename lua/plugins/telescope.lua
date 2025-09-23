030cebda169ef8acbda998dbe9d77180cd038bfe
Mose Mueller <mosemueller@gmail.com>
2025-09-23 13:29:34 +0200

    telescope: updates config and version

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 local M = {
   "nvim-telescope/telescope.nvim",
-  tag = "0.1.8",
+  commit = "b4da76b",
   cmd = { "Telescope" },
   dependencies = {
     {
@@ -9,35 +9,37 @@ local M = {
   },
 }
 
-local actions = require("telescope.actions")
 
-M.opts = {
-  defaults = {
-    prompt_prefix = " ",
-    selection_caret = " ",
-    path_display = { "smart" },
-    file_ignore_patterns = { ".git/", "node_modules" },
-    mappings = {
-      i = {
-        ["<Down>"] = actions.move_selection_next,
-        ["<Up>"] = actions.move_selection_previous,
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previous,
-        ["<C-u>"] = false, -- clear the prompt rather than scroll the previewer
+function M.setup()
+  local actions = require("telescope.actions")
+  return {
+    defaults = {
+      prompt_prefix = " ",
+      selection_caret = " ",
+      path_display = { "smart" },
+      file_ignore_patterns = { ".git/", "node_modules" },
+      mappings = {
+        i = {
+          ["<Down>"] = actions.move_selection_next,
+          ["<Up>"] = actions.move_selection_previous,
+          ["<C-j>"] = actions.move_selection_next,
+          ["<C-k>"] = actions.move_selection_previous,
+          ["<C-u>"] = false, -- clear the prompt rather than scroll the previewer
+        },
       },
     },
-  },
-  pickers = {
-    find_files = {
-      hidden = false, -- shows hidden files (starting with ".")
-      no_ignore = false, -- show files ignored by .gitignore, .ignore, etc.
-      no_ignore_parent = false, -- show files ignored by .gitignore, .ignore, etc. in parent dirs
+    pickers = {
+      find_files = {
+        hidden = false,           -- shows hidden files (starting with ".")
+        no_ignore = false,        -- show files ignored by .gitignore, .ignore, etc.
+        no_ignore_parent = false, -- show files ignored by .gitignore, .ignore, etc. in parent dirs
+      },
+      git_files = {
+        show_untracked = true, -- shows untracked files
+      },
     },
-    git_files = {
-      show_untracked = true, -- shows untracked files
-    },
-  },
-}
+  }
+end
 
 -- Falling back to find_files if git_files can't find a .git directory
 -- https://github.com/nvim-telescope/telescope.nvim/wiki/Configuration-Recipes#falling-back-to-find_files-if-git_files-cant-find-a-git-directory
