c25ca1ec62ba5c22cdd2cb598d39ce5d0fe1f8d9
Mose Mueller <mosemueller@gmail.com>
2025-09-23 13:31:04 +0200

    treesitter: updates to main branch

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,49 +1,104 @@
 local M = {
   "nvim-treesitter/nvim-treesitter",
-  commit = "e0d6c7643dc953acc2e817d0cebfc2f1f8c008e1",
-  event = "BufRead",
-  build = function()
-    require("nvim-treesitter.install").update({ with_sync = true })()
-  end,
-}
-
-function M.config()
-  require("nvim-treesitter.configs").setup({
-    modules = {},
-    sync_install = true, -- install languages synchronously (only applied to `ensure_installed`)
-    -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+  lazy = false,
+  commit = "5a70b1e",
+  build = ":TSUpdate",
+  ---@class TSConfig
+  opts = {
+    -- custom handling of parsers
     ensure_installed = {
       "bash",
-      "cpp",
+      "c",
+      "css",
+      "diff",
+      "dockerfile",
       "go",
       "html",
       "javascript",
+      "jsdoc",
+      "json",
       "jsonc",
+      "json5",
       "lua",
+      "luadoc",
+      "luap",
       "markdown",
       "markdown_inline",
       "python",
+      "regex",
       "toml",
-      "typescript",
       "tsx",
+      "typescript",
       "yaml",
     },
+  },
+  config = function(_, opts)
+    -- install parsers from custom opts.ensure_installed
+    if opts.ensure_installed and #opts.ensure_installed > 0 then
+      require("nvim-treesitter").install(opts.ensure_installed)
+      -- register and start parsers for filetypes
+      for _, parser in ipairs(opts.ensure_installed) do
+        local filetypes = parser -- In this case, parser is the filetype/language name
+        -- vim.treesitter.language.register(parser, filetypes)
 
-    -- Automatically install missing parsers when entering buffer
-    auto_install = true,
+        vim.api.nvim_create_autocmd({ "FileType" }, {
+          pattern = filetypes,
+          callback = function(event)
+            vim.treesitter.start(event.buf, parser)
+            vim.wo.foldmethod = 'expr'
+            vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+          end,
+        })
+      end
+    end
 
-    ignore_install = {}, -- List of parsers to ignore installing
+    -- Auto-install and start parsers for any buffer
+    vim.api.nvim_create_autocmd({ "BufRead" }, {
+      callback = function(event)
+          local bufnr = event.buf
+          local filetype = vim.api.nvim_get_option_value("filetype", { buf = bufnr })
 
-    highlight = {
-      enable = true, -- false will disable the whole extension
-      disable = {}, -- list of language that will be disabled
-    },
+          -- Skip if no filetype
+          if filetype == "" then
+              return
+          end
 
-    indent = {
-      enable = true,
-      disable = { "css" },
-    },
-  })
-end
+          -- Check if this filetype is already handled by explicit opts.ensure_installed config
+          for _, filetypes in pairs(opts.ensure_installed) do
+              local ft_table = type(filetypes) == "table" and filetypes or { filetypes }
+              if vim.tbl_contains(ft_table, filetype) then
+                  return -- Already handled above
+              end
+          end
+
+          -- Get parser name based on filetype
+          local parser_name = vim.treesitter.language.get_lang(filetype) -- might return filetype (not helpful)
+          if not parser_name then
+              return
+          end
+          -- Try to get existing parser (helpful check if filetype was returned above)
+          local parser_configs = require("nvim-treesitter.parsers")
+          if not parser_configs[parser_name] then
+              return -- Parser not available, skip silently
+          end
+
+          local parser_installed = pcall(vim.treesitter.get_parser, bufnr, parser_name)
+
+          if not parser_installed then
+              -- If not installed, install parser synchronously
+              require("nvim-treesitter").install({ parser_name }):wait(30000)
+          end
+
+          -- let's check again
+          parser_installed = pcall(vim.treesitter.get_parser, bufnr, parser_name)
+
+          if parser_installed then
+              -- Start treesitter for this buffer
+              vim.treesitter.start(bufnr, parser_name)
+          end
+      end,
+    })
+  end,
+}
 
 return M
