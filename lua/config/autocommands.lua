c44b46852785d775a069016e4f1effaa161b6669
Mose Mueller <mosemueller@gmail.com>
2025-10-07 17:34:18 +0200

    autocommands: adds LspAttach autocommand, moves autoformat into that

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -48,14 +48,6 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
   end,
 })
 
--- Enabling autoformat for certain file types
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-  pattern = { "*.py", "*.lua", "*.tsx", "*.ts", "*.go" },
-  callback = function()
-    vim.lsp.buf.format()
-  end,
-})
-
 -- Unfold everything
 -- vim.api.nvim_create_autocmd({ "BufEnter" }, {
 --   pattern = { "*" },
@@ -63,3 +55,105 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 --     vim.cmd("normal zR")
 --   end,
 -- })
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  desc = "LSP actions",
+  callback = function(args)
+    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+    if not client then
+      return
+    end
+
+
+    ---[[ Code Actions and Formatting on Save
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = vim.api.nvim_create_augroup("CodeAction and Format on Save", { clear = false }),
+      buffer = args.buf,
+      callback = function()
+        -- if vim.bo.ft == "python" then
+        --   vim.lsp.buf.code_action { context = { only = { "source.fixAll" }, diagnostics = {} }, apply = true }
+        -- end
+
+        if client:supports_method("textDocument/codeAction") then
+          local function apply_code_action(action_type)
+            local ctx = { only = action_type, diagnostics = {} }
+            local actions = vim.lsp.buf.code_action({ context = ctx, apply = true, return_actions = true })
+
+            -- only apply if code action is available
+            if actions ~= nil and #actions > 0 then
+              vim.lsp.buf.code_action({ context = ctx, apply = true })
+            end
+          end
+          apply_code_action({ "source.fixAll" })
+          -- apply_code_action({ "source.organizeImports" })
+        end
+
+        if not client:supports_method('textDocument/willSaveWaitUntil')
+            and client:supports_method('textDocument/formatting') then
+          vim.lsp.buf.format()
+        end
+      end,
+    })
+    ---]]
+
+    ---[[ Disable default formatting
+    if client.name == "tsserver" then
+      client.server_capabilities.documentFormattingProvider = false
+    end
+    ---]]
+
+    -- enable inlay hints (not sure if this is good here)
+    if vim.lsp.inlay_hint then
+      vim.lsp.inlay_hint.enable(true, { 0 })
+    end
+
+    if client:supports_method('textDocument/completion') then
+      vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+    end
+
+    if client:supports_method("textDocument/codeLens") then
+      vim.api.nvim_create_autocmd({ "TextChanged", "InsertLeave" }, {
+        buffer = args.buf,
+        callback = function()
+          vim.lsp.codelens.refresh({ bufnr = args.buf })
+        end,
+      })
+    end
+
+    ---[[ Lsp Keymaps
+    local nmap = function(keys, func, desc)
+      if desc then
+        desc = "LSP: " .. desc
+      end
+      vim.keymap.set("n", keys, func, { buffer = args.buf, noremap = true, silent = true, desc = desc })
+    end
+
+    nmap("K", function() vim.lsp.buf.hover { border = "rounded", max_height = 25, max_width = 120 } end, "Open hover")
+    nmap("gD", vim.lsp.buf.declaration, "Goto declaration")
+    nmap("gd", vim.lsp.buf.definition, "Goto definition")
+    nmap("gI", vim.lsp.buf.implementation, "List implementations")
+    nmap("gr", vim.lsp.buf.references, "List references")
+    nmap("<leader>la", vim.lsp.buf.code_action, "Code action")
+    nmap("<leader>lj", function()
+      vim.diagnostic.jump({ count = 1, float = true })
+    end, "Move to next diagnostic")
+    nmap("<leader>lk", function()
+      vim.diagnostic.jump({ count = -1, float = true })
+    end, "Move to previous diagnostic")
+    nmap("<leader>lr", vim.lsp.buf.rename, "Rename")
+    nmap("<F2>", vim.lsp.buf.rename, "Rename")
+
+    -- Diagnostic
+    vim.keymap.set("i", "<M-t>", function()
+      vim.lsp.buf.signature_help { border = "rounded", max_height = 25, max_width = 120 }
+    end, { buffer = args.buf })
+    nmap("<leader>lq", vim.diagnostic.setloclist, "Open diagnostics list")
+    nmap("gl", vim.diagnostic.open_float, "Open diagnostics float")
+
+    -- toogle inlay hints
+    nmap("<leader>lh", function()
+      vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+    end, "Toggle inlay hints")
+    ---]]
+  end,
+})
