50faaf35840ea0d7c2bb12224eac1ed3b19725d8
Mose Müller <mose.mueller@vertec.com>
2026-01-20 14:47:05 +0100

    options: add support for windows for vim.opt.shell

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,6 +4,18 @@ local sep = package.config:sub(0, 1)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+if vim.fn.has("win64") == 1 then
+  vim.opt.shell = vim.fn.executable "pwsh" and "pwsh" or "powershell"
+  vim.opt.shellcmdflag =
+  "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+  vim.opt.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
+  vim.opt.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+  vim.opt.shellquote = ""
+  vim.opt.shellxquote = ""
+else
+  vim.opt.shell = "sh"
+end
+
 vim.opt.number = true         -- set numbered lines
 vim.opt.relativenumber = true -- Show the line number relative to the line with the cursor in front of each line
 
