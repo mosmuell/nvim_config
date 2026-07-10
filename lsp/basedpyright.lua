e1b1718ebcd48ec46ae5653011c1edc095dcf0c6
Mose Mueller <mose.mueller@vertec.com>
2026-07-10 13:48:17 +0200

    lsp: add root_markers to basedpyright and ruff

--- lsp/basedpyright.lua
+++ lsp/basedpyright.lua
@@ -1,6 +1,7 @@
 return {
   cmd = { "basedpyright-langserver", "--stdio" },
   filetypes = { "python" },
+  root_markers = { "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", "Pipfile", ".git" },
   settings = {
     pyright = {
       -- Using Ruff's import organizer
--- lsp/ruff.lua
+++ lsp/ruff.lua
@@ -1,6 +1,7 @@
 return {
   cmd = { "ruff", "server" },
   filetypes = { "python" },
+  root_markers = { "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", "Pipfile", ".git" },
   init_options = {
     settings = {
       configurationPreference = "filesystemFirst",
